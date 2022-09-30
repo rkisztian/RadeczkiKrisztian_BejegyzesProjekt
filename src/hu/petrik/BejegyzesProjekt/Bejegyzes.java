5d2c31d11199f73cafd05b6b4973bf8faabd8561
Krisztián <radeczki.krisztian@gmail.com>
2022-09-30 14:24:17 +0200

    Létrhoztam a Projektet, és felvettem az adatokat

---
+++ src/hu/petrik/BejegyzesProjekt/Bejegyzes.java
@@ -0,0 +1,16 @@
+package hu.petrik.BejegyzesProjekt;
+
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+
+    private String szerzo;
+    private String tartalom;
+
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+
+
+}
