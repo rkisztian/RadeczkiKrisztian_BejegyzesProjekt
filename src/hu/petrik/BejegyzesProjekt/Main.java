9b54c26e9f41b64ff07107b4a7412190940c376e
Krisztián <radeczki.krisztian@gmail.com>
2022-09-30 15:20:50 +0200

    2. Feladat b része is kész van. Felhasználótol bekérunk adatokat, és ha hibás az adat, akkor hibát dob ki

--- src/hu/petrik/BejegyzesProjekt/Main.java
+++ src/hu/petrik/BejegyzesProjekt/Main.java
@@ -2,12 +2,38 @@ package hu.petrik.BejegyzesProjekt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
         static List<Bejegyzes> bejegyzesekLista = new ArrayList<>();
+        static Scanner sc = new Scanner(System.in);
 
         public static void main(String[] args){
             bejegyzesekHozzaadasa();
+
+            try {
+                bejegyzesFelvetele();
+            } catch (RuntimeException e){
+                System.out.println("A db szám csak pozitiv egész szám lehet.");
+            }
+
+        }
+
+        private static void bejegyzesFelvetele(){
+            System.out.println("Kérem adjon meg egy darabszámot: ");
+            int db =Integer.parseInt(sc.nextLine());
+            if(db < 0){
+                throw new NumberFormatException("Nem megfelelő értéket adott meg!");
+            }
+            for (int i = 0; i < db ; i++){
+                System.out.println("Kérem adja meg a bejegyzés szerzőjét.");
+                String szerzo = sc.nextLine();
+                System.out.println("Kérem adja meg a bejegyzés tartalmát ");
+                String tartalom = sc.nextLine();
+                Bejegyzes bejegyzes = new Bejegyzes(szerzo,tartalom);
+                bejegyzesekLista.add(bejegyzes);
+            }
+
         }
 
 
